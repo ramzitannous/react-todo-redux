c6b18a472796ded241fef03b9d9e95d0b973c913
ramzitannous <ramzi.tannous@exalt.ps>
2020-11-15 16:03:10 +0200

    Updates

---
+++ precache-manifest.84deda25a589c017db330efa4abb77b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8349de04a140d0a60c21428050f294c0",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "e1e9d5b2a499cfcb6ac2",
+    "url": "/react-todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4dda0eca46d55ac91bea",
+    "url": "/react-todo/static/css/main.23a3a28f.chunk.css"
+  },
+  {
+    "revision": "e1e9d5b2a499cfcb6ac2",
+    "url": "/react-todo/static/js/2.7e6928fa.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-todo/static/js/2.7e6928fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dda0eca46d55ac91bea",
+    "url": "/react-todo/static/js/main.17ce4eb1.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
